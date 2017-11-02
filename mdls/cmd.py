e225c4aa30cca601ea2c2cb0a6d94e281ac008aa
Park Hyunwoo <ez@smartstudy.co.kr>
2017-11-02 16:33:48 +0900

    Make a work

--- mdls/cmd.py
+++ mdls/cmd.py
@@ -1,25 +1,29 @@
 # -*- coding: utf-8 -*-
 import sys
 import argparse
+from mdls import sort_string
 
 
 def get_parser():
-    parser = argparse.ArgumentParser(
-        description='Markdown list sorter'
-    )
+    parser = argparse.ArgumentParser(description='Markdown list sorter')
 
-    parser.add_argument('infile', nargs=1, type=argparse.FileType('r'),
-                        default=sys.stdin)
-    parser.add_argument('outfile', nargs=1, type=argparse.FileType('w'),
-                        default=sys.stdout)
+    parser.add_argument('infile', type=argparse.FileType('r'), default=sys.stdin)
+    parser.add_argument('outfile', type=argparse.FileType('w'), default=sys.stdout)
+    parser.add_argument('-s', type=int, dest='sort_by', nargs='*',
+                        help='Set order by depth (1:ASC / -1:DESC / 0:Do not sort)')
 
     return parser
 
 
 def main():
     args = get_parser().parse_args()
-    print(args)
-    pass
+
+    source = args.infile.read()
+    result = sort_string(source, args.sort_by)
+
+    args.outfile.write(result)
+
+    sys.exit(0 if (source == result) else 1)
 
 
 if __name__ == '__main__':
