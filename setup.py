20eae1fd55d2aa451e722f769ff1ebceade3ada0
Park Hyun-woo <ez.amiryo@gmail.com>
2021-02-03 17:49:39 +0900

    Update Python versions for testing

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ classifiers = [
     'Intended Audience :: Developers',
     'Intended Audience :: System Administrators',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
     'License :: OSI Approved :: MIT License',
 ]
 
